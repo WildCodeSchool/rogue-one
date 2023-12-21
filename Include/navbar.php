cc072375558e4207c1f17391157f45a2ece19018
Joël MAYEMBA <joelmayemba@gmail.com>
2023-12-21 16:10:53 +0100

    update

---
+++ Include/navbar.php
@@ -0,0 +1,39 @@
+<head>
+	<title>Navbar</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Suranna&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<nav class="navbar navbar-expand-lg fixed-top navbar-scroll">
+    <div class="container">
+        <img src="lienimg" height="70" alt="" loading="lazy" />
+        <button class="navbar-toggler ps-0" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarExample01" aria-controls="navbarExample01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon d-flex justify-content-start align-items-center">
+                <i class="fas fa-bars"></i>
+            </span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarExample01">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" aria-current="page" href="#home">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#mocktails">Nos Mocktails</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#contact">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#aboutus">A propos</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include_once("../rogue-one/include/navbar.php");
+include_once("../rogue-one/include/products.php");
+
+
