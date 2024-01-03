0d26b844a50e17748828660f619c1a60648b21d6
chouaibj <99322580+chouaibj@users.noreply.github.com>
2024-01-03 19:04:53 +0100

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div id="footer-content">
+        <div id="copyright">
+            <p>&copy; <?php echo date("Y"); ?> Rogue-one. Tous droits réservés.</p>
+        </div>
+        <div id="mentions-legales">
+            <a href="mentions-legales.php">Mentions légales</a>
+        </div>
+    </div>
+</footer>
