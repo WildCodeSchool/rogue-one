6dac3c071a554c133263178342400ba7410036ea
Joël MAYEMBA <joelmayemba@gmail.com>
2024-01-04 17:17:50 +0100

    Revert "Merge branch 'dev' into 8-création-de-la-page-about"
    This reverts commit 99c0d5dd451dfa862e2a676ea63f450c0b9c4b82, reversing
    changes made to 78f95012851a685f0e7379247d0fb82be9f04996.

--- include/products.php
+++ include/products.php
@@ -1,136 +1,88 @@
 <?php
 $mocktails = [
     "WARA'TAIL" => [
-    "image" => "wara'Tail.png",
-    "category" => "energisants-naturels",
-    "prix" => "3,50€",
-    "detail" => "Boisson énergisante à l'Awara de BIO STRATÈGE (poudre concentrée), au Maracudja (Fruit de la Passion) et à la Mandarine. Sa belle couleur naturelle est grâce aux fruits qui la composent. Son goût est très original et bien fruité."
+        "image" => "wara'Tail.png",
+        "category" => "energisants-naturels"
     ],
-
     "WASSAÏ'TAIL" => [
-    "image" => "wassaï'Tail.png",
-    "category" => "energisants-naturels",
-    "prix" => "3,50€",
-    "detail" => "Boisson énergisante au Wassaï de BIO STRATÈGE (poudre concentrée) et à la Pitaya (Fruit du dragon) et au piment fort. Sa belle couleur naturelle est due aux fruits que contient ce Mocktail. Son goût est très original, fait penser au raisin et le piment chien sublime cette boisson."
+        "image" => "wassaï'Tail.png",
+        "category" => "energisants-naturels"
     ],
-
     "PANGA ROUJ'" => [
-    "image" => "pangaRouj'.png",
-    "category" => "fruits-locaux-epices",
-    "prix" => "3,50€",
-    "detail" => "Panga (Attention en créole guyanais) à son côté subtil, mais piquant avec le piment Chien. Sa belle couleur grâce au Pitaya. Son délicieux goût de Cerises Péyi."
+        "image" => "pangaRouj'.png",
+        "category" => "fruits-locaux-epices"
     ],
-
     "ICE COFFEE GINGER" => [
-    "image" => "iceCoffeeGinger.png",
-    "category" => "mariages-saveurs-uniques",
-    "prix" => "3,50€",
-    "detail" => "Café glacé guyanais parfumé au Gingembre et raffiné en bouche. Sa belle couleur grâce au Café. Son goût est unique et original."
+        "image" => "iceCoffeeGinger.png",
+        "category" => "mariages-saveurs-uniques"
     ],
-
     "MACOCI" => [
-    "image" => "macoci.png",
-    "category" => "fruits-locaux-epices",
-    "prix" => "3,50€",
-    "detail" => "Punch Coco sans Alcool bien crémeux, à base de Maracudja (Fruit de la Passion). Sa belle couleur grâce aux deux principaux fruits que contient ce mocktail. Son goût est très original et bien doux, rappelant une délicieuse pâtisserie fine."
+        "image" => "macoci.png",
+        "category" => "fruits-locaux-epices"
     ],
-
     "MORINAS" => [
-    "image" => "morinas.png",
-    "category" => "fraicheur-subtilite",
-    "prix" => "3,50€",
-    "detail" => "Boisson rafraîchissante à l'Ananas débordant de 'peps' avec sa Menthe bien fraîche et puissante. Sa belle couleur est due à l'Ananas et à la Menthe locale. Son goût est très fruité et mentholé, mais aussi relevé avec le bois d'Inde qui vient sublimer ce Mocktail."
+        "image" => "morinas.png",
+        "category" => "fraicheur-subtilite"
     ],
-
     "MÉLOJAH GINGER" => [
-    "image" => "melojaGinger.png",
-    "category" => "energisants-naturels",
-    "prix" => "3,50€",
-    "detail" => "Boisson bien rafraîchissante bien parfumée au gingembre, sans son côté piquant. Sa belle couleur est surtout due au Melon d’eau. Son goût est très fruité avec les discrets gingembre et maracudja, puis le melon d’eau qui nous affirme sa fraîcheur."
+        "image" => "melojaGinger.png",
+        "category" => "energisants-naturels"
     ],
-
     "HOT PAPA CHÉWI" => [
-    "image" => "hotPapaChewi.png",
-    "category" => "chaleur-epice",
-    "prix" => "3,50€",
-    "detail" => "Boisson bien chaleureuse avec son côté piquant avec le piment chien (fort). Sa belle couleur est due à la Papaye Solo et à la Cerise Péyi (Acerola). Son goût est très fruité et doux avec la Papaye qui apporte de la rondeur et adoucit le piquant du piment."
+        "image" => "hotPapaChewi.png",
+        "category" => "chaleur-epice"
     ],
-
     "SOLOJAH" => [
-    "image" => "solojah.png",
-    "category" => "fruits-locaux-epices",
-    "prix" => "3,50€",
-    "detail" => "Boisson débordant de 'peps' : à la fois frais et acidulé avec son maracudja. Sa belle couleur est due à la Papaye Solo et au Maracudja (Fruit de la Passion). Son goût est très fruité et doux avec la Papaye qui apporte de la rondeur : il nous fait drôlement penser à du planteur."
+        "image" => "solojah.png",
+        "category" => "fruits-locaux-epices"
     ],
-
     "PAPAYER PÉYI" => [
-    "image" => "papayerPeyi.png",
-    "category" => "fruits-d-excellence",
-    "prix" => "3,50€",
-    "detail" => "Boisson hyper rafraîchissante à la Papaye Solo, à la Citronnelle fraîche et au piment Scorpion. Sa belle couleur est grâce à la Papaye. Son délicieux goût de Thé Péyi fruité, raffiné et pimenté."
+        "image" => "papayerPeyi.png",
+        "category" => "fruits-d-excellence"
     ],
-
     "THÉ PÉYI DORÉ" => [
-    "image" => "thePeyiDore.png",
-    "category" => "mariages-saveurs-uniques",
-    "prix" => "3,50€",
-    "detail" => "Boisson hyper rafraîchissante, parfumée au Maracudja (Fruit de la Passion) et à la Citronnelle fraîche. Sa belle couleur grâce aux couleurs naturelles du Maracudja et de la Citronnelle. Son goût est très original, bien parfumé et subtilement relevé par le gingembre."
+        "image" => "thePeyiDore.png",
+        "category" => "mariages-saveurs-uniques"
     ],
-
     "HOT BLOOD" => [
-    "image" => "hotBlood.png",
-    "category" => "chaleur-epice",
-    "prix" => "3,50€",
-    "detail" => "Boisson bien rafraîchissante bien parfumée avec sa Cerise Péyi et sa Groseille Péyi. Sa belle couleur est due à ces deux fruits. Son goût est très fruité et agréablement acidulé, puis le piment fort vient sublimer notre Mocktail."
+        "image" => "hotBlood.png",
+        "category" => "chaleur-epice"
     ],
-
     "YANA START" => [
-    "image" => "yanaStart.png",
-    "category" => "fruits-d-excellence",
-    "prix" => "3,50€",
-    "detail" => "Boisson débordante de fraîcheur avec sa Mangue verte et sa Cerise Péyi, relevées par du Piment fort. Sa belle couleur est due au mariage de ces deux fruits. Son goût est rempli de caractère et reste tout de même frais."
+        "image" => "yanaStart.png",
+        "category" => "fruits-d-excellence"
     ],
 
-    // "ANANAS GINGER" => [
-    // "image" => "image_waratail.png",
-    // "category" => "saveurs-exotiques"
+    //   "ANANAS GINGER" => [
+    //     "image" => "image_waratail.png",
+    //     "category" => "saveurs-exotiques"
     // ],
-
     // "ANABELLA" => [
-    // "image" => "image_waratail.png",
-    // "category" => "saveurs-exotiques"
+    //     "image" => "image_waratail.png",
+    //     "category" => "saveurs-exotiques"
     // ],
-
     // "DÉTOX TROPI BANANE" => [
-    // "image" => "image_waratail.png",
-    // "category" => "fruits-locaux-epices"
+    //     "image" => "image_waratail.png",
+    //     "category" => "fruits-locaux-epices"
     // ],
-
     // "ICE COFFEE CHÉWI" => [
-    // "image" => "waratail.png",
-    // "category" => "mariages-saveurs-uniques"
+    //     "image" => "waratail.png",
+    //     "category" => "mariages-saveurs-uniques"
     // ],
-
     // "HOT MANDARINE À LA PASSION" => [
-    // "image" => "image_waratail.png",
-    // "category" => "saveurs-exotiques"
+    //     "image" => "image_waratail.png",
+    //     "category" => "saveurs-exotiques"
     // ],
-
     // "THÉ PANGA" => [
-    // "image" => "image_waratail.png",
-    // "category" => "fruits-locaux-epices"
+    //     "image" => "image_waratail.png",
+    //     "category" => "fruits-locaux-epices"
     // ],
-
     // "HOT MANGO TERRE" => [
-    // "image" => "image_waratail.png",
-    // "category" => "saveurs-exotiques"
+    //     "image" => "image_waratail.png",
+    //     "category" => "saveurs-exotiques"
     // ],
-
     // "CERANJA" => [
-    // "image" => "ceranja.png",
-    // "category" => "fraicheur-subtilite",
-    // "detail" => "Boisson hyper rafraîchissante à l’Orange Péyi et à la Cerise Péyi. Sa belle couleur est grâce à l’association de ces deux fruits. Son goût est délicieux et dépasse celui d’un jus d’orange nature."
-    // ]
+    //     "image" => "image_waratail.png",
+    //     "category" => "fraicheur-subtilite"
+    // ],
 ];
-
-?>
