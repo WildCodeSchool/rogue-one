bec7e2e2b0b06faa1411349a6b49fdd0fef079fd
Joël MAYEMBA <joelmayemba@gmail.com>
2024-01-04 09:38:41 +0100

    up

--- include/footer.php
+++ include/footer.php
@@ -1,14 +1,75 @@
-<footer>
-    <div class="container">
-        <div class="col-lg-12">
-            <div id="footer-content">
-                <div id="copyright">
-                    <p>&copy; <?php echo date("Y"); ?> Rogue-one. Tous droits réservés.Copyright © 2048</p>
+    <footer class="text-center text-lg-start mt-xl-5 pt-4">
+
+        <div class="container p-4">
+
+            <div class="row">
+
+                <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                    <h5 class="text-uppercase mb-4">OUR WORLD</h5>
+
+                    <ul class="list-unstyled mb-4">
+                        <li>
+                            <a href="#!" class="text-white">About us</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Collections</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Environmental philosophy</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Artist collaborations</a>
+                        </li>
+                    </ul>
                 </div>
-                <div id="mentions-legales">
-                    <a href="mentions-legales.php">Mentions légales</a>
+
+                <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                    <h5 class="text-uppercase mb-4">Assistance</h5>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!" class="text-white">Contact us</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Size Guide</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Shipping Information</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Returns & Exchanges</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Payment</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                    <h5 class="text-uppercase mb-4">Careers</h5>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!" class="text-white">Jobs</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+                    <h5 class="text-uppercase mb-4">S'abonner à la newsletter</h5>
+
+                    <div class="form-outline form-white mb-4">
+                        <input type="email" id="form5Example2" class="form-control" placeholder="Em@il" />
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Subscribe</button>
                 </div>
             </div>
+
         </div>
-    </div>
-</footer>
+
+        <div class="text-center p-3 border-top border-white">
+            © 2024 Copyright:
+            <a href="">Rogue One</a>
+        </div>
+    </footer>
