67f4cd619f871be710498d93db4a5d8da433f131
chouaibj <janahchouaib@gmail.com>
2024-01-04 15:36:08 +0100

    update 3

--- pages/mocktails.php
+++ pages/mocktails.php
@@ -20,17 +20,6 @@
 
 <body>
 
-  <div id="js-preloader" class="js-preloader">
-    <div class="preloader-inner">
-      <span class="dot"></span>
-      <div class="dots">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
-  </div>
-
   <?php include_once("../include/navbar.php") ?>
 
   <div class="page-heading header-text">
